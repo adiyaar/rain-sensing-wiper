8b7540f5afe901eec00340708fa1e1b6d67be9c2
Aditya Kashyap <65303764+adiyaar@users.noreply.github.com>
2021-12-07 14:39:16 +0530

    Update code.ino

--- code.ino
+++ code.ino
@@ -1,8 +1,8 @@
-#include <Servo.h>
-#include <Wire.h> 
-#include <LiquidCrystal_I2C.h>
 
-LiquidCrystal_I2C lcd(0x27, 2, 1, 0, 4, 5, 6, 7, 3, POSITIVE);
+#include <LiquidCrystal.h>
+#include <Servo.h>
+
+LiquidCrystal lcd(11,10,7,6,5,4);
 Servo myservo; 
 
 int pos = 0; 
@@ -14,70 +14,44 @@ void setup() {
   myservo.attach(9);
   lcd.clear();
   lcd.setCursor(0,0);
-  lcd.print("   AUTOMATIC "); 
+  lcd.print("Intelligent Rain"); 
   lcd.setCursor(0,1);
-  lcd.print("   CAR WIPER ");
-  delay(5000); 
+  lcd.print("Sensing CarViper");
+  delay(3000); 
+  lcd.clear();
+  lcd.setCursor(5,0);
+  lcd.print("Rainfall");
+  lcd.setCursor(0,1);
+  lcd.print("Intensity-");
 }
+
+
+
+void wipe(int Speed)
+{
+
+  if(Speed>400){Speed=5000;lcd.print("  LOW ");}
+  if(Speed>350&&Speed<=400){Speed=3000;lcd.print("MEDIUM");}
+  if(Speed<=350){Speed=500;lcd.print(" HIGH ");}
+  for (pos = 180; pos >= 0; pos--) {
+    myservo.write(pos);            
+    delay(3);                    
+  }
+  for (pos = 0; pos <= 180; pos++) { 
+    myservo.write(pos);             
+    delay(3);                       
+  }
+  delay(Speed);
+}
+
+
+
 
 void loop() 
 {
+lcd.setCursor(10,1);
 sensorValue = analogRead(A0);
 Serial.println(sensorValue);
-if(sensorValue>800){
-  myservo.write(180);
-  lcd.clear();
-  lcd.setCursor(0,0);
-  lcd.print("    NO RAIN ");
-  delay(1000);
-  }
-if(sensorValue<=800 && sensorValue>600){
-  lcd.print("AMOUNT: LOW ");
-  lcd.setCursor(0,0);
-  lcd.print("IT IS RAINING
-  ");
-  lcd.setCursor(0,1);
-  for (pos = 180; pos >= 0; pos-=1) {
-    myservo.write(pos);            
-    delay(3);                    
-  }
-  for (pos = 0; pos <= 180; pos+=1) { 
-    myservo.write(pos);             
-    delay(3);        
-  }
-  delay(2000);
-}
-
-if(sensorValue<=600 && sensorValue>460){
-  lcd.setCursor(0,0);
-  lcd.print("IT IS RAINING   ");
-  lcd.setCursor(0,1);
-  lcd.print("AMOUNT: MEDIUM ");
-  for (pos = 180; pos >= 0; pos-=1) {
-    myservo.write(pos);            
-    delay(3);                   
-  }
-  for (pos = 0; pos <= 180; pos+=1) { 
-    myservo.write(pos);             
-    delay(3);        
-  }
-  delay(1000);
-}
-
-if(sensorValue<460){
-  lcd.setCursor(0,0);
-  lcd.print("IT IS RAINING   ");
-  lcd.setCursor(0,1);
-  lcd.print("AMOUNT: HIGH    ");
-  for (pos = 180; pos >= 0; pos-=1) {
-    myservo.write(pos);            
-    delay(3);                    
-  }
-  for (pos = 0; pos <= 180; pos+=1) { 
-    myservo.write(pos);             
-    delay(3);        
-  }
-  delay(100);
-}
-
+if(sensorValue>600){myservo.write(180);lcd.print(" NIL  ");delay(1000);}
+if(sensorValue<=600){lcd.setCursor(10,1);wipe(sensorValue);}
 }
